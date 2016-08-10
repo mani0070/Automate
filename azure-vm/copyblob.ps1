1adddf49e0e1b9cb9d3524ddfee4985521b6a21d
Manimaran Chandrasekaran <mani@dinventive.com>
2016-08-10 20:46:03 +0100

    Create copyblob.ps1

---
+++ azure-vm/copyblob.ps1
@@ -0,0 +1,26 @@
+$rgName = 'name'
+$storageAccountName = 'dicustomimages'
+$customimagebloguri = 'https://<Storageaccountname>.blob.core.windows.net/system/Microsoft.Compute/Images/customvhds/dicustom-osDisk.a50ce7c4-0128-441e-a69f-bf712f4c4d93.vhd'
+$location = 'North Europe'
+#Create Below only if its first time
+New-AzureRmResourceGroup -Name $rgName -Location $location -Force
+
+$stoexists = Find-AzureRmResource -ResourceNameContains $storageAccountName 
+if ($stoexists -eq $null)
+{ 
+    New-AzureRmStorageAccount -ResourceGroupName $rgName -Name $storageAccountName -Location $location -SkuName Standard_LRS -Verbose
+    $stoctx = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey (Get-AzureRmStorageAccountKey -ResourceGroupName $rgName -Name $storageAccountName).Value[0] 
+    New-AzureStorageContainer -Name divhds  -Context $stoctx
+}
+else
+{ 
+$stoctx = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey (Get-AzureRmStorageAccountKey -ResourceGroupName $rgName -Name $storageAccountName).Value[0] 
+}
+Set-AzureStorageContainerAcl -Name divhds -Permission Blob -Context $stoctx
+$sto = Get-AzureRmStorageAccount -ResourceGroupName $rgName -Name $storageAccountName
+$stouri = -Join($sto.PrimaryEndpoints.Blob, 'divhds/dibasecustomimage' , '.vhd')
+$stouri
+
+Start-AzureStorageBlobCopy -AbsoluteUri $customimagebloguri -DestContainer 'divhds' -DestBlob 'dibasecustomimage.vhd' -DestContext $stoctx
+
+                         
